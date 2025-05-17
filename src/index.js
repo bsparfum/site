90846ec643ef874f9f31444627634c34a83ff017
bsparfum <maksn9466@gmail.com>
2025-05-17 18:52:32 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // 👈 Эту строку добавляем
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
